e9f91d8ca107ac639d36a6d2d8890c119140a849
nencyshah <shahnency2004@gmail.com>
2025-06-30 10:59:02 +0530

    create pages and route

--- RealEstate/src/App.jsx
+++ RealEstate/src/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './Home'
+import Signin from './Signin'
+import Signup from './Signup'
+import { Profiler } from 'react'
+import About from './About'
+import Profile from './Profile'
 
 export default function App() {
-  return (
-    <div className='text-red-600'>App</div>
-  )
+  return <BrowserRouter>
+  <Routes>
+   <Route path='/' element={<Home/>}/>
+   <Route path='/Signin' element={<Signin/>}/>
+   <Route path='/Signup' element={<Signup/>}/>
+   <Route path='/About' element={<About/>}/>
+   <Route path='/profile' element={<Profile/>}/>
+  </Routes>
+  </BrowserRouter>
 }
