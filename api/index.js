806225ad2167caafb4a67bd10e0d177711be1998
nencyshah <shahnency2004@gmail.com>
2025-07-01 10:56:06 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -11,3 +11,10 @@ const app = express();
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+
+//creating api routes
+
+api.get('/test', (req, res) => {//request is the data that we get from client side and response is the data that we send to client side
+ res.send('Hello World!'); //send is used to send the data to client side
+});
